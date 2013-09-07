8672a13cf81ed06de37513be75b7aa2b0ba0fd6b
Brendan Ferguson <bqf2000@yahoo.com>
2013-09-07 11:39:21 -0700

    Resizing images

--- app/controllers/statuses_controller.rb
+++ app/controllers/statuses_controller.rb
@@ -26,7 +26,7 @@ class StatusesController < ApplicationController
   # GET /statuses/new
   # GET /statuses/new.json
   def new
-    @status = Status.new
+    @status = current_user.statuses.new
     @status.build_document
 
     respond_to do |format|
